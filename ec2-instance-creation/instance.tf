c46ed1b5919f2730aef446f39486ffa75489ec23
Josiah <josiah_g@live.com>
2023-06-15 11:36:09 -0400

    Update instance.tf

--- ec2-instance-creation/instance.tf
+++ ec2-instance-creation/instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
   ami                    = "ami-0e820afa569e84cc1"
   instance_type          = "t2.micro"
   key_name               = "terraform-key"
-  vpc_security_group_ids = ["sg-0bddc005a474fe112"]
+  vpc_security_group_ids = ["YOUR_SECURITY_GROUP_ID"]
   tags = {
     Name = "terraform-ec2"
     Project = "Terraform"
